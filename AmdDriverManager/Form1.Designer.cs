980ce50ed30a85136c9a68738b1b5dc00dad7e56
o2Cloud <152266908+o2Cloud-fr@users.noreply.github.com>
2024-12-20 02:50:27 +0100

    v1.0

---
+++ AmdDriverManager/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace AmdDriverManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstDriverVersions = new System.Windows.Forms.ListBox();
+            this.btnUninstall = new System.Windows.Forms.Button();
+            this.lblDriverVersion = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(307, 432);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(147, 13);
+            this.linkLabel1.TabIndex = 23;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Visitez le site des pilotes AMD";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 428);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 13);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "AmdDriverManager - Version 1.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(528, 303);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "(Pour installer une nouvelle carte graphique)";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(487, 319);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(278, 41);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Nettoyer et étindre";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(556, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "(Risque de problème d\'écrn noir)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 253);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 41);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Nettoyer ET NE PAS redémarrer";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(574, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "(Fortement recommandé)";
+            // 
+            // lstDriverVersions
+            // 
+            this.lstDriverVersions.FormattingEnabled = true;
+            this.lstDriverVersions.Location = new System.Drawing.Point(12, 200);
+            this.lstDriverVersions.Name = "lstDriverVersions";
+            this.lstDriverVersions.Size = new System.Drawing.Size(421, 173);
+            this.lstDriverVersions.TabIndex = 14;
+            // 
+            // btnUninstall
+            // 
+            this.btnUninstall.Location = new System.Drawing.Point(487, 190);
+            this.btnUninstall.Name = "btnUninstall";
+            this.btnUninstall.Size = new System.Drawing.Size(278, 41);
+            this.btnUninstall.TabIndex = 13;
+            this.btnUninstall.Text = "Nettoyer et redémarrer";
+            this.btnUninstall.UseVisualStyleBackColor = true;
+            // 
+            // lblDriverVersion
+            // 
+            this.lblDriverVersion.AutoSize = true;
+            this.lblDriverVersion.Location = new System.Drawing.Point(141, 184);
+            this.lblDriverVersion.Name = "lblDriverVersion";
+            this.lblDriverVersion.Size = new System.Drawing.Size(80, 13);
+            this.lblDriverVersion.TabIndex = 12;
+            this.lblDriverVersion.Text = "lblDriverVersion";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::AmdDriverManager.Properties.Resources.AMD_Radeon_logo;
+            this.pictureBox2.Location = new System.Drawing.Point(617, 370);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(171, 68);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AmdDriverManager.Properties.Resources.AMD_Radeon_logo_2019;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(754, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lstDriverVersions);
+            this.Controls.Add(this.btnUninstall);
+            this.Controls.Add(this.lblDriverVersion);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "AmdDriverManager by o2Cloud";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lstDriverVersions;
+        private System.Windows.Forms.Button btnUninstall;
+        private System.Windows.Forms.Label lblDriverVersion;
+    }
+}
+
---
+++ AmdDriverManager/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace AmdDriverManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ShowStartupMessage();
+            LoadAmdDriverInfo();
+            ProcessArguments(Environment.GetCommandLineArgs());
+
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;  // Empêche le redimensionnement
+            this.MaximizeBox = false;  // Désactive le bouton de maximisation
+            this.MinimizeBox = false;  // Désactive le bouton de minimisation (optionnel)
+        }
+
+        private void ShowStartupMessage()
+        {
+            // Vérifie si l'application est en mode sans échec
+            if (!IsSafeMode())
+            {
+                MessageBox.Show(
+                    "AmdDriverManager a détecté que vous n'êtes PAS en mode sans échec...\n" +
+                    "Pour un nettoyage sans erreur, il est recommandé de redémarrer en mode sans échec.",
+                    "Attention",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+        }
+
+        private bool IsSafeMode()
+        {
+            // Vérifie si l'application est en mode sans échec en vérifiant une clé du registre.
+            string bootMode = Environment.GetEnvironmentVariable("SAFEBOOT");
+
+            // Si "SAFEBOOT" est défini dans l'environnement, nous sommes en mode sans échec.
+            return !string.IsNullOrEmpty(bootMode);
+        }
+
+        private void LoadAmdDriverInfo()
+        {
+            try
+            {
+                string finalVersion = "";
+
+                // Tentative 1: WMI
+                var amdDriverVersions = GetAmdDriverVersions();
+                if (amdDriverVersions.Count > 0)
+                {
+                    foreach (var version in amdDriverVersions)
+                    {
+                        lstDriverVersions.Items.Add(version);
+                        if (string.IsNullOrEmpty(finalVersion))
+                        {
+                            finalVersion = version;
+                        }
+                    }
+                }
+                else
+                {
+                    lstDriverVersions.Items.Add("No AMD drivers found via WMI.");
+                }
+
+                // Tentative 2: AMD command line tool (par exemple, `clinfo` ou autre)
+                try
+                {
+                    string amdSmiOutput = ExecuteAmdSmiCommand();
+                    if (!string.IsNullOrEmpty(amdSmiOutput))
+                    {
+                        if (string.IsNullOrEmpty(finalVersion))
+                        {
+                            finalVersion = $"Version (AMDSMI): {amdSmiOutput}";
+                        }
+                    }
+                }
+                catch
+                {
+                    // Continue si la commande AMD échoue
+                }
+
+                // Tentative 3: Registre
+                try
+                {
+                    string registryDriverVersion = GetAmdDriverVersionFromRegistry();
+                    if (!string.IsNullOrEmpty(registryDriverVersion))
+                    {
+                        if (string.IsNullOrEmpty(finalVersion))
+                        {
+                            finalVersion = $"Version (Registry): {registryDriverVersion}";
+                        }
+                    }
+                }
+                catch
+                {
+                    // Continue si la lecture du registre échoue
+                }
+
+                // Extraire et afficher uniquement les 6 derniers chiffres de la version
+                if (!string.IsNullOrEmpty(finalVersion))
+                {
+                    string versionToDisplay = finalVersion.Length >= 6 ? finalVersion.Substring(finalVersion.Length - 6) : finalVersion;
+                    lblDriverVersion.Text = $"AMD Driver {versionToDisplay}";
+                }
+                else
+                {
+                    lblDriverVersion.Text = "Aucune version de pilote AMD trouvée.";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading AMD driver info: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private List<string> GetAmdDriverVersions()
+        {
+            var driverVersions = new List<string>();
+            string query = "SELECT DriverVersion, DeviceName FROM Win32_PnPSignedDriver WHERE DeviceName LIKE '%AMD Radeon%'";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                string version = obj["DriverVersion"]?.ToString();
+                string deviceName = obj["DeviceName"]?.ToString();
+
+                if (!string.IsNullOrEmpty(version) && !string.IsNullOrEmpty(deviceName))
+                {
+                    driverVersions.Add($"Device: {deviceName}, Version: {version}");
+                }
+            }
+
+            return driverVersions;
+        }
+
+        private string ExecuteAmdSmiCommand()
+        {
+            try
+            {
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = "/c amd-smi --query-gpu=driver_version --format=csv,noheader",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = Process.Start(psi))
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+
+                    process.WaitForExit();
+
+                    if (process.ExitCode == 0 && !string.IsNullOrWhiteSpace(output))
+                    {
+                        return output.Trim();
+                    }
+                    else
+                    {
+                        throw new Exception($"amd-smi error: {error}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to execute amd-smi: {ex.Message}");
+            }
+        }
+
+        private string GetAmdDriverVersionFromRegistry()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\AMD\Driver"))
+                {
+                    if (key != null)
+                    {
+                        object driverVersion = key.GetValue("DriverVersion");
+                        if (driverVersion != null)
+                        {
+                            return driverVersion.ToString();
+                        }
+                    }
+                    throw new Exception("Driver version not found in the registry.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to get AMD driver version from registry: {ex.Message}");
+            }
+        }
+
+        private void DisplayDriverVersion()
+        {
+            try
+            {
+                string driverVersion = GetAmdDriverVersionFromRegistry();
+                lblDriverVersion.Text = $"AMD Driver Version: {driverVersion}";
+            }
+            catch (Exception ex)
+            {
+                lblDriverVersion.Text = $"Error: {ex.Message}";
+            }
+        }
+
+        // Méthode pour analyser les arguments de ligne de commande
+        private void ProcessArguments(string[] args)
+        {
+            // Vérifier si le tableau args contient suffisamment d'éléments avant d'accéder à args[1]
+            if (args.Length > 1)
+            {
+                if (args[1].ToLower() == "/uninstallrestart")
+                {
+                    ExecuteUninstallRestart();
+                }
+                else if (args[1].ToLower() == "/uninstallnorestart")
+                {
+                    ExecuteUninstallNoRestart();
+                }
+                else if (args[1].ToLower() == "/uninstallshutdown")
+                {
+                    ExecuteUninstallShutdown();
+                }
+            }
+            else
+            {
+                // Si les arguments sont insuffisants, afficher un message d'erreur ou prendre une autre action.
+                //MessageBox.Show("Arguments insuffisants. Veuillez fournir l'argument approprié.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ExecuteUninstallNoRestart()
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show(
+                    "Are you sure you want to uninstall the AMD driver without restarting?",
+                    "Confirm Uninstall",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    Process uninstallProcess = Process.Start("cmd.exe", "/c pnputil /delete-driver oem*.inf /uninstall /force");
+                    uninstallProcess.WaitForExit();
+                    MessageBox.Show("Uninstall command executed. No restart will be performed.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during uninstallation: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ExecuteUninstallShutdown()
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show(
+                    "Are you sure you want to uninstall the AMD driver and shut down the computer?",
+                    "Confirm Uninstall and Shutdown",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    Process uninstallProcess = Process.Start("cmd.exe", "/c pnputil /delete-driver oem*.inf /uninstall /force");
+                    uninstallProcess.WaitForExit();
+                    MessageBox.Show("Uninstall command executed. Your computer will shut down now.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Process.Start("shutdown", "/s /f /t 0");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during uninstallation: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void ExecuteUninstallRestart()
+        {
+            try
+            {
+                DialogResult result = MessageBox.Show(
+                    "Are you sure you want to uninstall the AMD driver and restart?",
+                    "Confirm Uninstall",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Warning
+                );
+
+                if (result == DialogResult.Yes)
+                {
+                    Process uninstallProcess = Process.Start("cmd.exe", "/c pnputil /delete-driver oem*.inf /uninstall /force");
+                    uninstallProcess.WaitForExit();
+                    MessageBox.Show("Uninstall command executed. Your computer will restart now.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Process.Start("shutdown", "/r /f /t 0");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error during uninstallation: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Ouvre l'URL dans le navigateur par défaut
+            System.Diagnostics.Process.Start("https://www.amd.com/fr/support/download/drivers.html");
+        }
+    }
+}
